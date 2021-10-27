10117d71a0939370d24a5a91b75ddda6a7e78d69
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-10-27 14:20:50 -0400

    get user information API done

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -3,13 +3,7 @@ const router = express.Router();
 const request = require("request");
 const path = require("path");
 
-/**
- * @swagger
- * /auth/login/:
- *   post:
- *     summary: gets information about a user
- *     description: Add later
-*/
+
 router.post("/login", async (req, res) => {
 	const credentials = {
 		email: req.body["email"],
@@ -32,9 +26,33 @@ router.post("/login", async (req, res) => {
 		if (err) {
 			res.status(400).send("Error while logging in " + error);
 		}
-		console.log("Login Sucessful", body);
 		res.status(201).send(response.body);
 	});
 });
+
+router.post('/getUser', async(req, res) => 
+{
+	const credentials = {
+		idToken: req.body["idToken"],
+	};
+	
+	const options = {
+		url:
+			"https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=" +
+			process.env.FIREBASEKEY,
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(credentials),
+	};
+
+	request.post(options, function (err, response, body) {
+		if (err) {
+			res.status(400).send("Error while getting user " + error);
+		}
+		res.status(201).send(body);
+	});
+})
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -36,36 +36,8 @@ for (const file of endpoints) {
     app.use('/api', endpoint);
 }
 
-// Setting up auto swaggerhub documentation 
-const swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-      title: 'U-Ride API documentation',
-      version: '1.0.0',
-      description:
-        'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
-      license: {
-        name: 'Licensed Under MIT',
-        url: 'https://spdx.org/licenses/MIT.html',
-      },
-    },
-    servers: [
-      {
-        url: 'http://localhost:5000',
-        description: 'Development server',
-      },
-    ],
-  };
-const options = {
-  swaggerDefinition,
-  // Paths to files containing OpenAPI definitions
-  apis: ['./api/*.js'],
-};
-
-const swaggerSpec = swaggerJSDoc(options);
 // Creating /api-docs path
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Creating /auth path 
 const AuthAPI = require('./api/auth');
