21cb4e3b67ec916c8fd1f91211d5feb693009712
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-11-03 18:20:43 -0400

    finalized basics for creating a carpool

--- backend/api/carpool.js
+++ backend/api/carpool.js
@@ -1,19 +1,30 @@
+const e = require("express");
 const express = require("express");
 const router = express.Router();
 const request = require("request");
 const mongoUtil = require("../mongoUtil");
 
 
+// Create Carpool 
 router.put("/insert", async (req, res) => {
     
     db = mongoUtil.get();
-
-    db.db("admin").collection("uride").insertOne(req.body, function (err) {
+    db.db("root").collection("uride").insertOne(req.body, function (err, response) {
         if (err) 
         {
-            throw err;
+           console.log("Error", err);
+           res.status(400).send(err);
+        }
+        else
+        {
+            res.status(200).send("New carpool created!");
         }
     });
 })
 
+// db.db("root").collection("uride").find({}).toArray(function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//     db.close();
+//   });
 module.exports = router;
