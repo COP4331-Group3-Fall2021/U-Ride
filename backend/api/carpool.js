64982323c9cdec2e401bb4413a959210efcf0c79
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-11-03 18:37:42 -0400

    removed unused imports

--- backend/api/carpool.js
+++ backend/api/carpool.js
@@ -1,7 +1,5 @@
-const e = require("express");
 const express = require("express");
 const router = express.Router();
-const request = require("request");
 const mongoUtil = require("../mongoUtil");
 
 
