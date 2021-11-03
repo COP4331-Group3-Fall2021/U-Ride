faf737b70771a35636995c2cc6c09366f05a5278
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-11-03 18:37:51 -0400

    reformatted code

--- backend/api/carpool.js
+++ backend/api/carpool.js
@@ -5,47 +5,40 @@ const mongoUtil = require("../mongoUtil");
 
 // Create Carpool 
 router.put("/insert", async (req, res) => {
-    
+
     db = mongoUtil.get();
     db.db("root").collection("uride").insertOne(req.body, function (err) {
-        if (err) 
-        {
-           res.status(400).send(err);
+        if (err) {
+            res.status(400).send(err);
         }
-        else
-        {
+        else {
             res.status(200).send("New carpool created!");
         }
     });
 })
 
 // Delete Carpool 
-router.delete("/delete", async(req,res) => 
-{
+router.delete("/delete", async (req, res) => {
     db = mongoUtil.get();
-    db.db("root").collection("uride").deleteOne(req.body, function(err){
-        if (err)
-        {
+    db.db("root").collection("uride").deleteOne(req.body, function (err) {
+        if (err) {
             res.status(400).send(err);
         }
-        else
-        {
+        else {
             res.status(200).send("Carpool deleted!");
         }
     });
 })
 
-router.get("/find", async(req,res) =>
-{
+router.get("/find", async (req, res) => {
     db = mongoUtil.get();
-    db.db("root").collection("uride").find({}).toArray(function(err, result) {
-        if (err) 
-        {
+    db.db("root").collection("uride").find({}).toArray(function (err, result) {
+        if (err) {
             res.status(400).send(err);
             throw err;
         }
         res.status(200).send(result);
-      });
+    });
 })
 
 module.exports = router;
