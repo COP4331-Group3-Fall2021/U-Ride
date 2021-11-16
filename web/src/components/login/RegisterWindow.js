ea775811660f48dcd4b3cb03cba503019c6ed564
Naton-1 <38639895+Naton-1@users.noreply.github.com>
2021-11-16 17:20:47 -0500

    Connected Register window to back end

--- web/src/components/login/RegisterWindow.js
+++ web/src/components/login/RegisterWindow.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import Button from '../Button';
+import { useHistory } from 'react-router-dom';
 const sha256 = require('js-sha256');
 
-function RegisterWindow ({goToLogin, goToForgotPassword}) {
+export default function RegisterWindow ({goToLogin, goToForgotPassword}) {
+    // Used to redirect to pages
+    const history = useHistory();
+
     var firstName;
     var lastName;
     var email;
@@ -19,57 +23,113 @@ function RegisterWindow ({goToLogin, goToForgotPassword}) {
             setMessage('Missing a field.');
 
             if (!validateInput(firstName.value)) {
-                document.getElementById("registerFirstName").classList.add('error');
+                document.getElementById("registerFirstName").classList.add('input-invalid');
+            } else {
+                document.getElementById("registerFirstName").classList.remove('input-invalid');
             }
             if (!validateInput(lastName.value)) {
-                document.getElementById("registerLastName").classList.add('error');
+                document.getElementById("registerLastName").classList.add('input-invalid');
+            } else {
+                document.getElementById("registerLastName").classList.remove('input-invalid');
             }
             if (!validateInput(email.value)) {
-                document.getElementById("registerEmail").classList.add('error');
+                document.getElementById("registerEmail").classList.add('input-invalid');
+            } else {
+                document.getElementById("registerEmail").classList.remove('input-invalid');
             }
             if (!validateInput(password.value)) {
-                document.getElementById("registerPassword").classList.add('error');
+                document.getElementById("registerPassword").classList.add('input-invalid');
+            } else {
+                document.getElementById("registerPassword").classList.remove('input-invalid');
             }
             if (!validateInput(confirmPassword.value)) {
-                document.getElementById("registerConfirmPassword").classList.add('error');
-            }
-            if (password.value !== confirmPassword.value) {
-                document.getElementById("registerConfirmPassword").classList.add('error');
+                document.getElementById("registerConfirmPassword").classList.add('input-invalid');
+            } else {
+                document.getElementById("registerConfirmPassword").classList.remove('input-invalid');
             }
             return;
         } else {
             setMessage('');
         }
+        if (password.value !== confirmPassword.value) {
+            setMessage('Passwords must match.');
+            document.getElementById("registerPassword").classList.add('input-invalid');
+            document.getElementById("registerConfirmPassword").classList.add('input-invalid');
+            return;
+        } else {
+            setMessage('');
+            document.getElementById("registerPassword").classList.remove('input-invalid');
+            document.getElementById("registerConfirmPassword").classList.remove('input-invalid');
+        }
         
-        // Hash password, then construct request body
+        // Hash password, then construct HTTP request
         var hash = sha256(password.value);
-        var obj = {firstName:firstName.value, lastName:lastName.value, email:email.value, password:hash};
-        var json = JSON.stringify(obj);
+        var headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        var requestBody = JSON.stringify({
+            name:{
+                firstName:firstName.value,
+                lastName:lastName.value
+            },
+            email:email.value,
+            password:hash
+        });
+        var requestOptions = {
+            method: 'POST',
+            headers: headers,
+            body: requestBody,
+            redirect: 'follow'
+        };
+
+        // Send Register API request and handle server response
+        fetch('https://u-ride-cop4331.herokuapp.com/auth/register', requestOptions)
+            .then(response => response.text())
+            .then(result => {
+                // Handle error messages
+                if (result === 'EMAIL_EXISTS') {
+                    setMessage('Account already exists.');
+                }
+                else if (result === 'TOO_MANY_ATTEMPTS_TRY_LATER') {
+                    setMessage('Google says: f*** you.');
+                }
+                // Handle success
+                else {
+                    let responseBody = JSON.parse(result);
+                    let userInfo = {firstName:responseBody.name.firstName, lastName:responseBody.name.lastName, userID:responseBody._id}
+                    setMessage('');
+
+                    // Set user info into local storage
+                    localStorage.setItem('user_data', JSON.stringify(userInfo));
+
+                    // Redirect user to homepage
+                    history.push('/home');
+                }
+            })
+            .catch(error => console.error('error', error));
 
         // Send API request: Login
-        try {
-            const response = await fetch('https://u-ride-cop4331.herokuapp.com/api/register',
-                {method:'POST',body:json,headers:{'Content-Type': 'application/json'}});
+        // try {
+        //     const response = await fetch('https://u-ride-cop4331.herokuapp.com/auth/register', requestOptions);
 
-            var res = JSON.parse(await response.text());
+        //     var res = JSON.parse(await response.text());
 
-            // Check if valid login
-            if (res.id <= 0) {
-                setMessage('Invalid login.');
-            }
-            else {
-                var user = {firstName:res.firstName, lastName:res.lastName, id:res.id}
-                localStorage.setItem('user_data', JSON.stringify(user));
+        //     // Check if valid login
+        //     if (res.id <= 0) {
+        //         setMessage('Invalid login.');
+        //     }
+        //     else {
+        //         var user = {firstName:res.firstName, lastName:res.lastName, id:res.id}
+        //         localStorage.setItem('user_data', JSON.stringify(user));
 
-                // Clear error and input fields
-                setMessage('');
-                window.location.href = '/home'; // !!!!!!!!!!!!!!!! This is important to note
-            }
-        }
-        catch(e) {
-            alert(e.toString());
-            return;
-        }    
+        //         // Clear error and input fields
+        //         setMessage('');
+        //         window.location.href = '/home'; // !!!!!!!!!!!!!!!! This is important to note
+        //     }
+        // }
+        // catch(e) {
+        //     alert(e.toString());
+        //     return;
+        // }    
     }
 
     return (
@@ -106,5 +166,3 @@ function validateInput (input) {
         return true;
     }
 }
-
-export default RegisterWindow;
