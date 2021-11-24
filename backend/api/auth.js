2aa05bbebbf7f1ecda72d2b99883f3cb6673ee9e
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-11-24 13:15:41 -0500

    added credientials as enviornemtn variables

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -7,12 +7,24 @@ const mongoUtil = require ('../mongoUtil');
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./poosd-610ed-firebase-adminsdk-qltca-47a7eb34ec.json");
+
+var serviceAccount = 
+{
+	"type": "service_account",
+	"project_id": process.env.PROJECT_ID,
+	"private_key_id":process.env.PRIVATE_KEY_ID,
+	"private_key": process.env.PRIVATE_KEY,
+	"client_email": process.env.CLIENT_EMAIL,
+	"client_id": process.env.CLIENT_ID,
+	"auth_uri": process.env.AUTH_URI,
+	"token_uri": process.env.TOKEN_URI,
+	"auth_provider_x509_cert_url": process.env.AUTH_URL,
+	"client_x509_cert_url": process.env.CERT_URL
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-
 
 async function checkEmailVerification(token, res, successResults)
 {
