b2d7a203efa5761bf16ff36f2b9b5e4b0cd93c09
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-12-04 21:52:07 -0500

    added ellie's tests

--- backend/testing/carpool_tests.js
+++ backend/testing/carpool_tests.js
@@ -3,6 +3,7 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const expect = chai.expect
 const baseUrl = "https://u-ride-cop4331.herokuapp.com/"
+
 // const baseUrl = "localhost:3200/"
 chai.use(chaiHttp);
 describe("Carpool ", function(){
@@ -38,7 +39,45 @@ describe("Carpool ", function(){
 
 // Ellie
 it('find carpool', function(done) {
-
+    chai.request(baseUrl)
+    .post('carpool/find')
+    .send({
+        "_id": "string",
+        "numParticipants": 2,
+        "maxParticipants": 3,
+        "poolDate": "2021-11-18T21:27:12+0000",
+        "origin": [
+          [
+            "longitude",
+            "latitude"
+          ]
+        ],
+        "destination": [
+          [
+            "longitude",
+            "latitude"
+          ]
+        ],
+        "riders": [
+          [
+            "6196c6217dcae90022e067e8",
+            "6196c6457dcae90022e067e9"
+          ]
+        ],
+        "driver": {
+          "_id": "6196c6627dcae90022e067ea",
+          "name": {
+            "firstName": "test",
+            "lastName": "user"
+          }
+        },
+        "isFull": false
+      })
+    .end(function (err, res) {
+        expect(res).to.have.status(200);
+        expect(err).to.be.null;
+        done();
+    });
 });
 
 // Vic
@@ -62,7 +101,45 @@ it('find rides', function(done){
 // Ellie
 it('find drives', function(done)
 {
-
+    chai.request(baseUrl)
+    .post('carpool/findDrives')
+    .send( {
+        "_id": "string",
+        "numParticipants": 2,
+        "maxParticipants": 3,
+        "poolDate": "2021-11-18T21:27:12+0000",
+        "origin": [
+          [
+            "longitude",
+            "latitude"
+          ]
+        ],
+        "destination": [
+          [
+            "longitude",
+            "latitude"
+          ]
+        ],
+        "riders": [
+          [
+            "6196c6217dcae90022e067e8",
+            "6196c6457dcae90022e067e9"
+          ]
+        ],
+        "driver": {
+          "_id": "6196c6627dcae90022e067ea",
+          "name": {
+            "firstName": "test",
+            "lastName": "user"
+          }
+        },
+        "isFull": false
+      })
+    .end(function (err, res) {
+        expect(res).to.have.status(200);
+        expect(err).to.be.null;
+        done();
+    });
 })
 
 // Vic
@@ -82,7 +159,45 @@ it('join carpool', function(done)
 // Ellie
 it('update carpool', function(done)
 {
-
+    chai.request(baseUrl)
+    .post('carpool/update')
+    .send( {
+        "_id": "string",
+        "numParticipants": 2,
+        "maxParticipants": 3,
+        "poolDate": "2021-11-18T21:27:12+0000",
+        "origin": [
+          [
+            "longitude",
+            "latitude"
+          ]
+        ],
+        "destination": [
+          [
+            "longitude",
+            "latitude"
+          ]
+        ],
+        "riders": [
+          [
+            "6196c6217dcae90022e067e8",
+            "6196c6457dcae90022e067e9"
+          ]
+        ],
+        "driver": {
+          "_id": "6196c6627dcae90022e067ea",
+          "name": {
+            "firstName": "test",
+            "lastName": "user"
+          }
+        },
+        "isFull": false
+      })
+    .end(function (err, res) {
+        expect(res).to.have.status(200);
+        expect(err).to.be.null;
+        done();
+    });
 })
 
 // Vic
@@ -100,6 +215,16 @@ it('leave carpool', function(done)
 
 // Ellie
 it('delete carpool', function(done) {
+    chai.request(baseUrl)
+    .post('carpool/delete')
+    .send( {
+        "_id": "string"
+      })
+    .end(function (err, res) {
+        expect(res).to.have.status(200);
+        expect(err).to.be.null;
+        done();
+    });
 });
 
 })
